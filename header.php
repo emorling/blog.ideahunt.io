ae29c2acda1d9a3ab6a0fa9688e7056261229d85
Elia Mörling <elia.morling@gmail.com>
2016-04-22 14:25:15 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/wp-content/themes/ideahunt/css/main.css">
+	<link rel="stylesheet" href="/wp-content/themes/ideahunt/css/custom.css">
+	<script src="//code.tidio.co/kunpbxrezl6pnxq6bdgevuybipujkmap.js"></script>
+	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+</head>
+
+
+<body <?php body_class(); ?>>
+
+<div class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" data-toggle="collapse" data-target=".navbar-collapse" class="navbar-toggle">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a href="http://ideahunt.io" class="navbar-brand"><img src="http://www.ideahunt.io/assets/images/logo.svg" style="height:30px;margin-top: -5px;"></a>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="/">Home</a></li>
+				<li><a href="/faq">FAQ</a></li>
+			</ul>
+			<?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
+					<button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'twentysixteen' ); ?></button>
+
+					<div id="site-header-menu" class="site-header-menu">
+						<?php if ( has_nav_menu( 'primary' ) ) : ?>
+							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
+								<?php
+									wp_nav_menu( array(
+										'theme_location' => 'primary',
+										'menu_class'     => 'primary-menu',
+									 ) );
+								?>
+							</nav><!-- .main-navigation -->
+						<?php endif; ?>
+
+						<?php if ( has_nav_menu( 'social' ) ) : ?>
+							<nav id="social-navigation" class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentysixteen' ); ?>">
+								<?php
+									wp_nav_menu( array(
+										'theme_location' => 'social',
+										'menu_class'     => 'social-links-menu',
+										'depth'          => 1,
+										'link_before'    => '<span class="screen-reader-text">',
+										'link_after'     => '</span>',
+									) );
+								?>
+							</nav><!-- .social-navigation -->
+						<?php endif; ?>
+					</div><!-- .site-header-menu -->
+				<?php endif; ?>
+			<ul class="nav navbar-nav navbar-right"></ul>
+		</div>
+	</div>
+</div>
+
+<div id="page" class="site container">
+	<div class="site-inner">
+		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
+
+		<div id="content" class="site-content">
